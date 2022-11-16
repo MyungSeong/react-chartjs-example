124b65e1e51731c0e8e51dfaaa7c683006adfda7
MyungSeong <moves01@naver.com>
2022-11-16 10:54:29 +0900

    🔨 [UPDATE] Date Picker 라이브러리 변경

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
-import ChartElement from './ChartElement';
-import DatePickerElement from './DatePickerElement';
+import ChartComponents from './ChartComponents';
+import DatePickerComponents from './DatePickerComponents';
+
+import { i18n } from './utils';
 
 const Main = () => {
     const [date, setDate] = useState();
 
+    /* const { t } = useTranslation();
+
+    if (router.query['lang'] && typeof router.query.lang === 'string') {
+        i18n.changeLanguage(lang);
+    }
+
+    document.documentElement.setAttribute('lang', i18n.language); */
+
     return (
         <>
-            <DatePickerElement setDate={setDate} />
-            <ChartElement date={date} />
+            <DatePickerComponents setDate={setDate} />
+            <ChartComponents date={date} />
+            {/* {t('Test')} */}
         </>
     );
 };
